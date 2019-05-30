5f8f96ba51efa6fd20d76a2abdfcde538be334c0
brenj <bwhite@nowsecure.com>
2019-05-30 11:41:01 -0400

    Remove unused configurations in truffle config

--- truffle.js
+++ truffle.js
@@ -1,11 +1,6 @@
-// var HDWalletProvider = require("truffle-hdwallet-provider");
-// var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
-
 module.exports = {
   networks: {
     development: {
-      // provider: function() {
-      //   return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
       host: "127.0.0.1",
       port: 8545,
       network_id: "*",
