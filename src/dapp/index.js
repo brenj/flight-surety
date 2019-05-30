e1fafac6fe4e0090dbe33cd4ce75d04d23dd1b82
brenj <bwhite@nowsecure.com>
2019-05-30 15:47:14 -0400

    Add some quick front-end styling

--- src/dapp/index.js
+++ src/dapp/index.js
@@ -11,7 +11,7 @@ import './flightsurety.css';
 
         // Read transaction
         contract.isOperational((error, result) => {
-            display('Operational Status', 'Check if contract is operational', [ { label: 'Operational Status', error: error, value: result} ]);
+            display('', '', [ { label: 'Operational status:', error: error, value: result} ]);
         });
 
         // User-submitted transaction
@@ -19,7 +19,7 @@ import './flightsurety.css';
             let flight = DOM.elid('flight-number').value;
             // Write transaction
             contract.fetchFlightStatus(flight, (error, result) => {
-                display('Oracles', 'Trigger oracles', [ { label: 'Fetch Flight Status', error: error, value: result.flight + ' ' + result.timestamp} ]);
+                display('Oracles', 'Trigger oracles', [ { label: 'Fetch Flight Status', error: error, value: result.flight + ', ' + result.timestamp} ]);
             });
         })
     
@@ -45,15 +45,21 @@ import './flightsurety.css';
 
 function display(title, description, results) {
     let displayDiv = DOM.elid("display-wrapper");
+    let resultsDiv = DOM.elid("results-wrapper");
     let section = DOM.section();
     section.appendChild(DOM.h2(title));
     section.appendChild(DOM.h5(description));
     results.map((result) => {
         let row = section.appendChild(DOM.div({className:'row'}));
-        row.appendChild(DOM.div({className: 'col-sm-4 field'}, result.label));
-        row.appendChild(DOM.div({className: 'col-sm-8 field-value'}, result.error ? String(result.error) : String(result.value)));
+        row.appendChild(DOM.div({className: 'col-sm-2 field'}, result.label));
+        row.appendChild(DOM.div({className: 'col-sm-10 field-value'}, result.error ? String(result.error) : String(result.value)));
         section.appendChild(row);
     })
-    displayDiv.append(section);
 
+  if (results[0].label === 'Operational status:') {
+    displayDiv.append(section);
+  } else {
+    resultsDiv.innerHTML = '';
+    resultsDiv.append(section);
+  }
 }
